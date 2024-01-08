0fc599e0d76dcf9b4767093927e298c2c733ea3f
ggrossman618 <60393057+ggrossman618@users.noreply.github.com>
2024-01-07 21:38:38 -0500

    Finished Retaliation Play Logic

--- retaliationPlayLogic.py
+++ retaliationPlayLogic.py
@@ -79,22 +79,22 @@ teamMappings = {
 
 # Running analytics per team and creating graphs
 #Offensive Formations
-# for key, value in teamMappings.items():
-#     if not teamMappings[key].empty:
-#         offenseFormationPercentages = teamMappings[key]['offenseFormation'].value_counts(normalize=True) * 100
-#         plt.figure(figsize=(8, 6))
-#         offenseFormationPercentages.plot(kind='bar', color='skyblue')
-#         plt.ylim(0, 100)  # Set the y-axis limit to 100
-#         plt.title(f"{key}'s Retaliation Play Formation Percentage")
-#         plt.xlabel('Offensive Formation')
-#         plt.ylabel('Percentage')
-#         plt.xticks(rotation=0)  # Rotate x-labels if needed
-#         plt.tight_layout()
-#         plt.show()
+for key, value in teamMappings.items():
+    if not teamMappings[key].empty:
+        offenseFormationPercentages = teamMappings[key]['offenseFormation'].value_counts(normalize=True) * 100
+        plt.figure(figsize=(8, 6))
+        offenseFormationPercentages.plot(kind='bar', color='skyblue')
+        plt.ylim(0, 100)  # Set the y-axis limit to 100
+        plt.title(f"{key}'s Retaliation Play Formation Percentage")
+        plt.xlabel('Offensive Formation')
+        plt.ylabel('Percentage')
+        plt.xticks(rotation=0)  # Rotate x-labels if needed
+        plt.tight_layout()
+        plt.show()
 
-# Machine learning model for predicting offensive formations
+# # Machine learning model for predicting offensive formations
 
-teamPredictability = {} # Rank teams' predictability based on model accuracy
+teamFormPredictability = {} # Rank teams' predictability based on model accuracy
 
 for team, value in teamMappings.items():
     print('*------------------------------------*')
@@ -103,7 +103,7 @@ for team, value in teamMappings.items():
     
     # Setting up data for model
     teamName = team
-    team = teamMappings[team]
+    team = teamMappings[team].copy()
     team['possessionTeam'] = team['possessionTeam'].map(mappings.teamMappings)
     team['defensiveTeam'] = team['defensiveTeam'].map(mappings.teamMappings)
     team['yardlineSide'] = team['yardlineSide'].map(mappings.teamMappings)
@@ -165,15 +165,100 @@ for team, value in teamMappings.items():
     print("Classification Report:")
     print(classification_report(y_test , y_pred))
     
-    teamPredictability[teamName] = round(accuracy_score(y_test, y_pred, sample_weight=None), 2)
+    teamFormPredictability[teamName] = round(accuracy_score(y_test, y_pred, sample_weight=None), 2)
     
 
 print('*------------------------------------*')
 print('')
 
 # Sorting team predictibility dictionary, then converting to dataframe
-teamPredictability = dict(sorted(teamPredictability.items(), key=lambda item: item[1]))
-teamPredictability = {'Team': list(teamPredictability.keys()), 'Predictability': list(teamPredictability.values())}
-teamPredictability = pd.DataFrame(teamPredictability)
+teamFormPredictability = dict(sorted(teamFormPredictability.items(), key=lambda item: item[1]))
+teamFormPredictability = {'Team': list(teamFormPredictability.keys()), 'Predictability': list(teamFormPredictability.values())}
+teamFormPredictability = pd.DataFrame(teamFormPredictability)
 print('Team Predictability Rankings:')
-print(teamPredictability)
+print(teamFormPredictability)
+
+
+# Play Type
+print('')
+print('*** PLAY TYPE ***')
+print('')
+
+# Running analytics per team and creating graphs
+#Offensive Play Types
+for key, value in teamMappings.items():
+    if not teamMappings[key].empty:
+        # set running plays to 0 and passing plays to 1
+        teamMappings[key]['playTypeGraph'] = teamMappings[key]['passResult'].apply(lambda x: 0 if pd.isna(x) else 1)
+        teamMappings[key]['playTypeGraph'] = teamMappings[key]['playTypeGraph'].apply(lambda x: 'Run' if x == 0 else 'Pass')
+
+        offensePlayPercentages = teamMappings[key]['playTypeGraph'].value_counts(normalize=True) * 100
+        plt.figure(figsize=(8, 6))
+        offensePlayPercentages.plot(kind='bar', color='skyblue')
+        plt.ylim(0, 100)  # Set the y-axis limit to 100
+        plt.title(f"{key}'s Retaliation Play Type Percentage")
+        plt.xlabel('Offensive Play')
+        plt.ylabel('Percentage')
+        plt.xticks(rotation=0)  # Rotate x-labels if needed
+        plt.tight_layout()
+        plt.show()
+
+teamPlayPredictability = {} # Rank teams' predictability based on model accuracy
+
+for team, value in teamMappings.items():
+    print('*------------------------------------*')
+    print(team)
+    print('*-------------------------------------*')
+    
+    # Setting up data for model
+    teamName = team
+    team = teamMappings[team].copy()
+    team['possessionTeam'] = team['possessionTeam'].map(mappings.teamMappings)
+    team['defensiveTeam'] = team['defensiveTeam'].map(mappings.teamMappings)
+    team['yardlineSide'] = team['yardlineSide'].map(mappings.teamMappings)
+    team['offenseFormation'] = team['offenseFormation'].map(mappings.offensiveFormationsMappings)
+    team['gameClock'] = team['gameClock'].apply(getSeconds)
+    
+    # set running plays to 0 and passing plays to 1
+    team['playType'] = team['passResult'].apply(lambda x: 0 if pd.isna(x) else 1)
+    
+    team = team.drop(['playResult', 'defendersInTheBox', 'ballCarrierDisplayName', 'expectedPoints', 'ballCarrierId', 'playDescription', 'passResult', 'passLength', 'foulName1', 'foulName2', "foulNFLId1", 'foulNFLId2', 'penaltyYards', 'playNullifiedByPenalty', 'prePenaltyPlayResult', 'expectedPointsAdded', 'homeTeamWinProbabilityAdded', 'visitorTeamWinProbilityAdded', 'playTypeGraph'], axis=1)
+    team = team.dropna()
+
+    y = team['playType']
+    X = team.drop(['playType'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) # use test size 25 for individual teams
+    
+    # Train the model
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(X_train, y_train)
+
+    # Make predictions
+    y_pred = model.predict(X_test)
+    y_pred_proba = model.predict_proba(X_test)
+
+     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # Print accuracy of model
+    print("The model's accuracy is: ", np.round(accuracy_score(y_test, y_pred, sample_weight=None) * 100, 4), '%')
+    print('')
+
+    comparisonDf = pd.DataFrame({
+        'Actual': y_test,
+        'Predicted': y_pred
+    })
+
+    comparisonDf['Actual'] = comparisonDf['Actual'].apply(lambda x: 'Run' if x == 0 else 'Pass')
+    comparisonDf['Predicted'] = comparisonDf['Predicted'].apply(lambda x: 'Run' if x == 0 else 'Pass')
+
+    print(comparisonDf)
+
+    teamPlayPredictability[teamName] = round(accuracy_score(y_test, y_pred, sample_weight=None), 2)
+
+# Sorting team predictibility dictionary, then converting to dataframe
+teamPlayPredictability = dict(sorted(teamPlayPredictability.items(), key=lambda item: item[1]))
+teamPlayPredictability = {'Team': list(teamPlayPredictability.keys()), 'Predictability': list(teamPlayPredictability.values())}
+teamPlayPredictability = pd.DataFrame(teamPlayPredictability)
+print('')
+print('Team Play Predictability Rankings:')
+print(teamPlayPredictability)
